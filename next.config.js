87e4a8a32245a276944ee54b8eec82778f99b006
anungbisa4 <anung.bisa4@gmail.com>
2020-12-17 17:46:46 +0700

    init

---
+++ next.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  // Uncomment the line below to enable basePath, pages and
+  // redirects will then have a path prefix (`/app` in this case)
+  //
+  basePath: '/anung-ganteng',
+
+  async redirects() {
+    return [
+      {
+        source: '/anung-ganteng',
+        destination: '/anung-ganteng',
+        permanent: true,
+      },
+      {
+        source: '/team',
+        destination: '/about',
+        permanent: false,
+      },
+      // Path Matching - will match `/old-blog/a`, but not `/old-blog/a/b`
+      {
+        source: '/old-blog/:slug',
+        destination: '/news/:slug',
+        permanent: false,
+      },
+      // Wildcard Path Matching - will match `/blog/a` and `/blog/a/b`
+      {
+        source: '/blog/:slug*',
+        destination: '/news/:slug*',
+        permanent: false,
+      },
+      // Regex Path Matching - The regex below will match `/post/123` but not `/post/abc`
+      {
+        source: '/post/:slug(\\d{1,})',
+        destination: '/news/:slug',
+        permanent: false,
+      },
+    ]
+  },
+}
